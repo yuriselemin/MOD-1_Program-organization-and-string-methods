90e9f8666ee6dbd8dd728aab626992faccbf75c9
Selemin Yuriy <71262890+yuriselemin@users.noreply.github.com>
2024-05-19 22:48:29 +0300

    Add files via upload

---
+++ MOD-1_homework3.py
@@ -0,0 +1,12 @@
+
+# Практическая работа по уроку "Организация программ и методы строк."
+
+
+my_string = input('Введите фразу: ')
+
+print(len(my_string))
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
